9be21a919f22943d3be3b2edc8c9f5f1fb526101
Enric <enricorillosa@hotmail.es>
2022-10-17 13:19:31 +0200

    #2-3

--- basics/src/App.vue
+++ basics/src/App.vue
@@ -1,10 +1,29 @@
 <template>
   <Navbar />
+    <button @click="show()">Calcular</button>
+    <div v-if="isRender" class="calculator"><input type="number"> + <input type="number"> = {{result}}
+</div>
   <Footer />
 </template>
 <script setup>
 import  Navbar from "./components/Navbar.vue";
 import  Footer from "./components/Footer.vue";
+import {ref} from 'vue';
+const isRender = ref(false);
+const show = () => {
+  isRender.value = !isRender.value;
+}
 </script>
 <style scoped>
+
+input {
+  width: 50px;
+}
+button {
+margin-left: 50%;
+margin-bottom: 10px;
+}
+.calculator {
+margin-left: 48%;
+}
 </style>
